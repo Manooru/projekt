e82829d1285287f2c0af135b60b855f880d5ae4b
Manooru <148362024+Manooru@users.noreply.github.com>
2024-01-29 17:59:01 +0100

    Update Kalkulator.java

--- Kalkulator.java
+++ Kalkulator.java
@@ -1,40 +1,6 @@
-import java.util.Scanner;
-
-public class Main {
+public class Kalkulator {
     public static void main(String[] args) {
-        System.out.println("Kalkulator, ver 0.00a. " +
-                "Funkcja jedynie przez kosnolę na czas obecny i ograniczoną liczbę działań do dwóch");
-        Scanner writer = new Scanner(System.in);
-        int c;
-        System.out.println("Wpisz cyfrę: ");
-        int a = writer.nextInt();
-
-        System.out.println("Wpisz znak: ");
-        var type = writer.next();
-
-        System.out.println("Wpisz liczbę: ");
-        int b = writer.nextInt();
-
-        switch (type) {
-            case "+":
-                c = a + b;
-                System.out.println("Twój wynik to: " + c);
-                break;
-            case "-":
-                c = a - b;
-                System.out.println("Twój wynik to: " + c);
-                break;
-            case "*":
-                c = a * b;
-                System.out.println("Twój wynik to: " + c);
-                break;
-            case "/":
-                c = a / b;
-                System.out.println("Twój wynik to: " + c);
-                break;
-
-            default:
-                System.out.println("Wprowadziłeś nieodpowiedni znak. Akceptowalne znaki to: '+', '-', '*', '/'");
-        }
+        System.out.println("Kalkulator wersja 1.00a."+"\nWitaj w kalkulatorze!");
+        Obliczanie.Oblicz();
     }
 }
