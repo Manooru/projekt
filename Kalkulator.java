446b31ac2bada03661bb87b4975a5f58df9c9bae
Manooru <148362024+Manooru@users.noreply.github.com>
2024-04-08 20:27:09 +0200

    Update Kalkulator.java

--- Kalkulator.java
+++ Kalkulator.java
@@ -1,6 +1,6 @@
 public class Kalkulator {
     public static void main(String[] args) {
-        System.out.println("Kalkulator wersja 1.00a."+"\nWitaj w kalkulatorze!");
+        System.out.println("Kalkulator wersja 1.2a."+"\nWitaj w kalkulatorze!");
         Obliczanie.Oblicz();
     }
 }
