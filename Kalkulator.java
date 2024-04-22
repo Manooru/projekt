49fa9b03e521fadcc8c81bcd2d1b43bfe61e348b
Manooru <148362024+Manooru@users.noreply.github.com>
2024-04-22 17:53:47 +0200

    Delete Kalkulator.java

--- Kalkulator.java
+++
@@ -1,6 +0,0 @@
-public class Kalkulator {
-    public static void main(String[] args) {
-        System.out.println("Kalkulator wersja 1.2a."+"\nWitaj w kalkulatorze!");
-        Obliczanie.Oblicz();
-    }
-}
